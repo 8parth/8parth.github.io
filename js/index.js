e4c4a3c5df83758e12a72e097a0bc96c611c3671
Parth <parthmodi54@yahoo.com>
2016-07-03 18:50:52 +0530

    added smoothscrolling and section detection, improved contact me section

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$('body').scrollspy({
+  target: '#navigation'
+});
+/* for smooth scrolling effect */
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+/* for back to top button */
+
+$(window).scroll(function() {
+  console.log("running");
+	if ( $(window).scrollTop() > 300 ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
